0c6e38eb1805ede0486c4df68bb9e615865fc350
Edward Tanguay (hm.win) <edward@tanguay.info>
2022-01-28 15:59:58 +0100

    created class

--- js/StarComponent.js
+++ js/StarComponent.js
@@ -1,5 +1,31 @@
-export const setStars = (number) => {
-	createStars(number);
-	localStorage.setItem('stars', number);
+export class StarComponent {
+	constructor(document) {
+		this.document = document;
+		this.starsElem = this.document.querySelector('.stars');
+		this.numberElem = this.document.querySelector('.number');
+	}
+
+	setStars(number) {
+		this.createStars(number);
+		localStorage.setItem('stars', number);
+	}
+
+	createStars(number) {
+		this.starsElem.innerHTML = '';
+		for (let i = 1; i <= number; i++) {
+			this.starsElem.innerHTML += `<i onclick="setStars(${i})" class="fa fa-star"></i>`;
+		}
+		for (let i = number + 1; i <= 5; i++) {
+			this.starsElem.innerHTML += `<i onclick="setStars(${i})" class="fa fa-star-o"></i>`;
+		}
+		this.numberElem.innerText = number;
+	}
+
+	render() {
+		const cachedStars = Number(localStorage.getItem('stars'));
+		const number = cachedStars === null ? 3 : cachedStars;
+		this.createStars(number);
+	}
+
 }
 
--- js/main.js
+++ js/main.js
@@ -1,20 +1,4 @@
-import { setStars } from './StarComponent.js';
+import { StarComponent } from './StarComponent.js';
 
-const starsElem = document.querySelector('.stars');
-const numberElem = document.querySelector('.number');
-
-const cachedStars = Number(localStorage.getItem('stars'));
-const number = cachedStars === null ? 3 : cachedStars;
-
-const createStars = (number) => {
-	starsElem.innerHTML = '';
-	for (let i = 1; i <= number; i++) {
-		starsElem.innerHTML += `<i onclick="setStars(${i})" class="fa fa-star"></i>`;
-	}
-	for (let i = number + 1; i <= 5; i++) {
-		starsElem.innerHTML += `<i onclick="setStars(${i})" class="fa fa-star-o"></i>`;
-	}
-	numberElem.innerText = number;
-}
-
-createStars(number);
+const starComponent = new StarComponent(document);
+starComponent.render();
