5d1c3437449f6c23705e4d42a7ff274a00f4a32a
Edward Tanguay (hm.win) <edward@tanguay.info>
2022-01-28 15:03:24 +0100

    number changes

--- js/main.js
+++ js/main.js
@@ -5,11 +5,17 @@ const number = 3;
 
 // stars
 for (let i = 1; i <= number; i++) {
-	starsElem.innerHTML += '<i class="fa fa-star"></i>';
+	starsElem.innerHTML += `<i onclick="setStars(${i})" class="fa fa-star"></i>`;
 }
-for (let i = 1; i <= 5 - number; i++) {
-	starsElem.innerHTML += '<i class="fa fa-star-o"></i>';
+for (let i = number + 1; i <= 5; i++) {
+	starsElem.innerHTML += `<i onclick="setStars(${i})" class="fa fa-star-o"></i>`;
 }
 
 // number
 numberElem.innerText = number;
+
+// functions
+
+const setStars = (number) => {
+	numberElem.innerText = number;
+}
